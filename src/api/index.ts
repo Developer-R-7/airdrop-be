78419d857bc360f59c5c34554ce9c47c8f93ce81
Rushi Patel <rushinasa06@gmail.com>
2023-04-16 16:45:57 +0530

    :bug: fix imports

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import tasksRouter from './tasks/tasks-router';
+import userRouter from './user/user-router';
+import companyRouter from './company/company-router';
+
 export default (): Router => {
   const app = Router();
-  app.use('/user', UserRouter);
+  app.use('/user', userRouter);
+  app.use('/company', companyRouter);
   app.use('/tasks', tasksRouter());
 
   return app;
