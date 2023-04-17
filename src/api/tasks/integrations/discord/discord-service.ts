a08bd89f5fd1c137a9d163909d4d001d571f1fc1
Rushi Patel <rushinasa06@gmail.com>
2023-04-17 15:19:15 +0530

    :bug: fix bug for discord auth

--- src/api/tasks/integrations/discord/discord-service.ts
+++ src/api/tasks/integrations/discord/discord-service.ts
@@ -27,23 +27,26 @@ export const verifyGuild = async (authCode: any, companyDiscordGuildID: string) 
     );
     const accessToken = tokenResponse.data.access_token;
     const userGuildsID = await getUserGuilds(accessToken);
-    const foundGuild = userGuildsID.find((guild: any) => guild.id === companyDiscordGuildID);
-    if (foundGuild) {
-      return true;
-    } else {
-      return false;
+    let flag = false;
+
+    for (let i = 0; i < userGuildsID.length; i++) {
+      if (userGuildsID[i] === companyDiscordGuildID.toString()) {
+        flag = true;
+        break;
+      }
     }
+
+    return flag;
   } catch (err) {
     throw { code: err.code || 500, message: err.message || 'Verifying guild failed' };
   }
 };
 
 export const getCompanyDiscordGuildID = async (companyID: string) => {
-  console.log(companyID);
   const db = (await dbClient()).collection('communities');
   const getCompanyProfile = await db.findOne({ _id: new ObjectId(companyID) });
   if (!getCompanyProfile) {
     throw { code: 404, message: 'Company not found.' };
   }
-  return getCompanyProfile.social_handles.discord.guildId;
+  return getCompanyProfile.social_handles.discord.guildId.toString();
 };
